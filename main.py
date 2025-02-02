3b677d121f3c4d282085cf56151b0fbe21418e3c
John Horgan <johnhorgan12@hotmail.com>
2025-02-02 14:01:03 +0000

    mainbasic

---
+++ main.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+
+app = Flask(__name__)
+
+# create database
+
+
+# create routes
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
